642d4f0dd04076cc72c0076ac08705c3b4fcca1a
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-04-12 20:58:51 +0900

    ADD:Moives and actors

---
+++ movies/models.py
@@ -0,0 +1,30 @@
+from platform import release
+from pyexpat import model
+from tkinter import CASCADE
+from turtle import ondrag
+from django.db import models
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        db_table = 'actors'
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    running_time = models.IntegerField()
+    actor = models.ManyToManyField(Actor, through="ActorMovie")
+
+    class Meta:
+        db_table = 'movies'
+
+class ActorMovie(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'actors_movies'
+
